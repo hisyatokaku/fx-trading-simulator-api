280ffcc09c91085378c17f85b1e8ab69c54c53d7
Kein Yukiyoshi <yukiyoshikein@gmail.com>
2026-09-08 20:21:03 +0900

    chore: add one-pager for day2

--- scripts/convert_md_to_pdf.sh
+++ scripts/convert_md_to_pdf.sh
@@ -3,4 +3,4 @@ cd document \
   --launch-options='{"args":["--no-sandbox"]}' \
   --basedir=. \
   --css='img { max-width: 100%; height: auto; }' \
-  how_to_use_colab.md
+  /home/ky2001/OneDrive/Linux/fx-trading-simulator-api/document/day2/one-pager.md
